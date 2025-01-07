8e357357159cda25e5464489a5b0e7ddb99a89a4
pfaj <pfaj2004@gmail.com>
2025-01-07 18:26:06 -0500

    home: added info sections to the homepage

---
+++ src/components/info/InfoSection.js
@@ -0,0 +1,17 @@
+function InfoSection({heading, text, image}) {
+  return (
+      <>
+           <div className="infoSection">
+               <div className="infoText"> 
+                    <h2>{heading}</h2>
+                    <p>{text}</p>
+               </div>
+               <div className="infoImage">
+                   <img src={image} alt=""/>
+               </div>
+           </div> 
+      </>
+  );
+}
+
+export default InfoSection;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,32 @@
 import Layout from '../components/Layout.js'
 import Hero from '../components/hero/Hero.js'
 import ClientScroll from '../components/home/ClientScroll.js'
+import InfoSection from '../components/info/InfoSection.js'
 
 function Project(){
     return(
         <Layout>
             <Hero text="SEISMIC STUDIOS" heroSubText="making a ground-breaking impact"/>
             <ClientScroll/>
+
+            <div className='infoContainer'>
+                <InfoSection heading="ABOUT US" 
+                    text="We transform ideas into compelling stories through the power of video. As creative
+                    problem-solvers and visual storytellers, we partner with our clients to deliver 
+                    messages that leave a lasting impact."
+
+                    image=""
+                />
+
+                <InfoSection heading="OUR SERVICES" 
+                    text="Our seasoned team handles every aspect of production - from concept development and scriptwriting
+                    to filming, editing, and post-production. We dig deep to understand your unique challenges,
+                    creatively crafting media that achieves your goals."
+
+                    image=""
+                />
+            </div>
+
         </Layout>
     )
 }
